dca161d88dc9ca06a60fbade98faaa206f54e881
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-03-18 11:53:50 -0400

    Completed palindrome exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //create empty variable to reverse the input string
+            var reversedWord = "";
+
+            //take input string word and loop backwards
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversedWord += word[i];
+            }
+            //now check if word and reversedWord are equal and return true else return false
+            if (word == reversedWord)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,28 @@
-using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        //Add [Theory] keyword
+        //and [InlineData()] -----below with input test data
+        [Theory]
+        [InlineData("tacocat", true)]
+        [InlineData("racecar", true)]
+        [InlineData("computer", false)]
 
+        //create method to test
+        public void PalindromeCheck(string input, bool check)
+        {
+            //Arrange - create new object
+            var palindromeTest = new WordSmith();
+
+            //Act - store object.method in actual variable
+            var actual = palindromeTest.IsAPalindrome(input);
+
+            //Assert
+            Assert.Equal(check, actual);
         }
     }
 }
